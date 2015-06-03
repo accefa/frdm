0c20e7bce99f86dad9a0c86c78644874decbe7a0
Nino Ninux <nino.ninux@gmail.com>
2015-06-03 15:00:38 +0200

    improved STP init

--- projects/Shell/Sources/STP.c
+++ projects/Shell/Sources/STP.c
@@ -145,8 +145,49 @@ void STP_en(int status)
 
 void STP_reset(void)
 {
+	uint8_t res = ERR_OK;
+	l6480_cmd_resetdevice();
+
+	l6480_reg_step_mode_t stepmode_config;
+	stepmode_config.reg.step_sel = L6480_STEP_SEL_MICRO_128;                //128 steps
+	stepmode_config.reg.sync_sel = 0;               //sync unimportant due to BUSY - mode
+	stepmode_config.reg.sync_en = 0;                //Busy mode
+	l6480_set_step_mode(stepmode_config.raw.data);
+
+	l6480_reg_config_t config;
+	config.reg.osc_sel = 0;  //unused
+	config.reg.ext_clk = 0;  //unused
+	config.reg.sw_mode = 1;  //user disposal
+	config.reg.en_vscomp  = 0; //Bridge shut down
+	config.reg.oc_sd   = 1;     //Bridge shutdown
+	config.reg.uvloval = 0;     //6.9V,6.3V etc
+	config.reg.vccval  = 0;     //7.5V
+	config.reg.f_pwm_dec = 4;
+	config.reg.f_pwm_int = 1;
+	l6480_set_config(config.raw.data);
+
+	l6480_set_ocd_th_millivolt(1000);           // Overcurrentdetection Treshold
+	l6480_set_stall_th_millivolt(1000);         // Stalldetection Tresold
+	l6480_set_gatecfg1_igate_milliampere(96);   // Gatestrom
+	l6480_set_gatecfg1_tcc_nanosecond(250);     // Bestromungszeiten
+	l6480_set_gatecfg1_tboost_nanosecond(125);
+	l6480_set_gatecfg2_tdt_nanosecond(250);
+	l6480_set_gatecfg2_tblank_nanosecond(250);  // Pausenzeit Messung
+	l6480_set_kval_hold(16);                        // KVAL Motor Stillstand
+	l6480_set_kval_run(128);
+	l6480_set_kval_acc(128);
+		l6480_set_kval_dec(128);
+
+	l6480_set_fs_spd(0x07FF);
+	l6480_cmd_hardstop();
+
+	l6480_cmd_softhiz();
+
 	l6480_set_kval_run(128);
 	l6480_set_kval_acc(128);
 	l6480_set_kval_dec(128);
+
+	// l6480_cmd_gohome();
+
 	STP_en(STP_EN_OFF);
 }
